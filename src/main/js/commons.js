30e18a58475c7bfbd6b44f1909a6b7b964a07e6c
Mariano Ruiz <mrsarm@gmail.com>
2020-05-11 20:00:17 -0300

    Improve `getTargetValue()`. Minor import refactor

--- src/main/js/commons.js
+++ src/main/js/commons.js
@@ -3,21 +3,19 @@
  * @param target can be the target object from an event (event.target) or
  * any form DOM element that have a value, like a input element (HTMLInputElement),
  * a select element (HTMLSelectElement)...
- * @returns the string value, or an array if it's a multi-value element
+ * @returns the string value, an array of strings if it's a multi-value element,
+ *          and if target is a checkbox component the boolean value
  */
 function getTargetValue(target) {
-  let value
-  if (target.options) { // <select> with or without multiple values
-    value = []
-    for (let i = 0; i < target.options.length; i++) {
-      if (target.options[i].selected) {
-        value.push(target.options[i].value)
-      }
+  if (target.type === "checkbox") return target.checked
+  if (!target.options) return target.value
+  const values = []
+  for (let i = 0; i < target.options.length; i++) {
+    if (target.options[i].selected) {
+      values.push(target.options[i].value)
     }
-  } else {
-    value = target.value
   }
-  return value
+  return values
 }
 
 /**
