5560671408e414ca0bf28ac7200eef0b86ae5e7d
Mariano Ruiz <mrsarm@gmail.com>
2020-05-18 18:27:29 -0300

    Improvements in `getTargetValue(...)` and `applyEventToState(...)`

--- src/main/js/commons.js
+++ src/main/js/commons.js
@@ -1,3 +1,5 @@
+import _  from 'lodash'
+
 /**
  * Gets the value from the target event.
  * @param target can be the target object from an event (event.target) or
@@ -8,14 +10,19 @@
  */
 function getTargetValue(target) {
   if (target.type === "checkbox") return target.checked
-  if (!target.options) return target.value
-  const values = []
-  for (let i = 0; i < target.options.length; i++) {
-    if (target.options[i].selected) {
-      values.push(target.options[i].value)
+  if (target.type === "select-multiple") {
+    const values = []
+    for (let i = 0; i < target.options.length; i++) {
+      if (target.options[i].selected) {
+        values.push(target.options[i].value)
+      }
     }
+    return values
   }
-  return values
+  if (target.type === "select-one" && target.value === "") {
+    return null
+  }
+  return target.value
 }
 
 /**
@@ -51,16 +58,20 @@ function getTargetValue(target) {
  * @param event with the value, it will be processed with
  *        `#getTargetValue(target)` in case it has multi-values.
  * @param state the current state
- * @param stateElName the name of the element to change at the root
- *        of the state
+ * @param stateElName the key at the root of the state to change,
+ *        eg. "user", but it can be also a path eg. if `stateElName` is
+ *        "user.profile" and `event.target.name` is "username", the
+ *        value from the `event` object will be applied to
+ *        the path `state.user.profile.username`.
  * @param setState the function to change the state
  */
 function applyEventToState(event, state, stateElName, setState) {
+  const stateEl = _.cloneDeep(state[stateElName])
   const name = event.target.name
   const value = getTargetValue(event.target)
-  const stateEl = {...state[stateElName]}
-  stateEl[name] = value
-  setState({[stateElName]: stateEl})
+  _.set(stateEl, name, value)
+  const stateElNameRoot = stateElName.split('.')[0]
+  setState({[stateElNameRoot]: stateEl})
 }
 
 export {getTargetValue, applyEventToState}
