99130ef5ebcfb4c4cf363843684cfd6611d5f4a3
Mariano Ruiz <mrsarm@gmail.com>
2020-05-21 16:10:30 -0300

    Nulls instead of empty values in React states

--- src/main/js/commons.js
+++ src/main/js/commons.js
@@ -3,12 +3,14 @@ import _  from 'lodash'
 /**
  * Gets the value from the target event.
  * @param target can be the target object from an event (event.target) or
- * any form DOM element that have a value, like a input element (HTMLInputElement),
- * a select element (HTMLSelectElement)...
+ *        any form DOM element that have a value, like a input element (HTMLInputElement),
+ *        a select element (HTMLSelectElement)...
+ * @param emptyToNull (default false), set to true if empty string "" should be
+ *        replaced by `null`
  * @returns the string value, an array of strings if it's a multi-value element,
  *          and if target is a checkbox component the boolean value
  */
-function getTargetValue(target) {
+function getTargetValue(target, emptyToNull = false) {
   if (target.type === "checkbox") return target.checked
   if (target.type === "select-multiple") {
     const values = []
@@ -20,6 +22,9 @@ function getTargetValue(target) {
     return values
   }
   if (target.type === "select-one" && target.value === "") {
+    return null
+  }
+  if (emptyToNull && target.value === "") {
     return null
   }
   return target.value
@@ -55,6 +60,8 @@ function getTargetValue(target) {
  *       applyEventToState(event, this.state, "user", this.setState.bind(this))
  *     }
  *
+ * Empty string values "" are replaced by null.
+ *
  * @param event with the value, it will be processed with
  *        `#getTargetValue(target)` in case it has multi-values.
  * @param state the current state
@@ -68,7 +75,7 @@ function getTargetValue(target) {
 function applyEventToState(event, state, stateElName, setState) {
   const stateEl = _.cloneDeep(state[stateElName])
   const name = event.target.name
-  const value = getTargetValue(event.target)
+  const value = getTargetValue(event.target, true)
   _.set(stateEl, name, value)
   const stateElNameRoot = stateElName.split('.')[0]
   setState({[stateElNameRoot]: stateEl})
