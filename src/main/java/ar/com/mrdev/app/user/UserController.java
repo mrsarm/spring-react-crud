01ebaf3c9ecba75cb6c85304af1d32f8386c516e
Mariano Ruiz <mrsarm@gmail.com>
2023-02-23 19:05:02 -0300

    Remove unused imports

--- src/main/java/ar/com/mrdev/app/user/UserController.java
+++ src/main/java/ar/com/mrdev/app/user/UserController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 the original author or authors.
+ * Copyright 2015-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  */
 package ar.com.mrdev.app.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
